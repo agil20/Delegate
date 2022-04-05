3268a6f6640ca51681b5376fba2740661f2af7c2
agil20 <agilakh@code.edu.az>
2022-04-05 15:18:04 +0400

    Add Hw

---
+++ DeleGete/DeleGete/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DeleGete
+{
+    internal class Program
+    {
+        delegate bool Check(int num);
+        delegate void Write(string str1);
+        delegate int Topla(int num1, int num2);
+
+        static void Main(string[] args)
+        {
+            Topla topla = delegate (int n, int m)
+            {
+                return n + m;
+            };
+            topla+=nu
+        }
+    }
+}
